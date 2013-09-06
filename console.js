77e9dd36753cbfdd9a2784cfe2d4a4d68e0fb111
ccmitchellusa <ccmitchellusa@gmail.com>
2013-09-05 21:19:55 -0400

    Remove copyrights

--- console.js
+++ console.js
@@ -1,11 +1,4 @@
 //-------------------------------------------------------------------------
-//
-// Licensed Materials - Property of IBM
-// (C) Copyright IBM Corp. 2012  All Rights Reserved
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-//
-//-------------------------------------------------------------------------
 // Module: dwl/console
 //-------------------------------------------------------------------------
 define([
