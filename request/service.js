2317ed366e3983d9e995a4a3f7e5556443e88dea
ccmitchellusa <ccmitchellusa@gmail.com>
2013-09-05 21:20:51 -0400

    Remove copyrights

--- request/service.js
+++ request/service.js
@@ -1,13 +1,4 @@
 //---------------------------------------------------------------------------------
-//
-// IBM Software Services for WebSphere (ISSW)
-//
-// Licensed Materials - Property of IBM
-// (C) Copyright IBM Corp. 2009  All Rights Reserved
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-//
-//---------------------------------------------------------------------------------
 // Module: issw/mobile/worklight/service
 //---------------------------------------------------------------------------------
 require([
