6b5f3affa0b9be18d56af236a89be45d1fc91175
ccmitchellusa <ccmitchellusa@gmail.com>
2013-09-05 21:32:26 -0400

    Cleanup

--- console.js
+++ console.js
@@ -1,23 +1,17 @@
-//-------------------------------------------------------------------------
-// Module: dwl/console
-//-------------------------------------------------------------------------
 define([
-    "dojo/_base/array",          // arr
-    "dojo/_base/json",           // json
-    "dojo/_base/lang",
-    "dojo/aspect",
-    "dojo/has",
-
-    "./has/worklight"
+	"dojo/_base/array",
+	"dojo/_base/json",
+	"dojo/_base/lang",
+	"dojo/aspect",
+	"dojo/has",
+	"./has/worklight"
 ], function(array, json, lang, aspect, has) {
 
-	//--------------------------------------------------------------------
 	var MODULE = "dwl/console";
 	var hc = {
 		_init        : false
 	};
 
-	//--------------------------------------------------------------------
 	// Set up list style console output. Hybrids only output first argument :(
 	if ( !hc._init ) {
 		hc._init = true;
@@ -51,13 +45,13 @@ define([
 			aspect.before(console   , "error"   , _logProcess);
 
 			if ( has("worklight") ) {
-				//-- Fix brain dead WL logger to support multiple args
+				// Fix brain dead WL logger to support multiple args
 				aspect.before(WL.Logger , "debug"   , _logProcess);
 				aspect.before(WL.Logger , "error"   , _logProcess);
 			}
 
 			if ( has("worklight-android") ) {
-				//-- Redefine console to Worklight's Logger (for cleaner LogCat output)
+				// Redefine console to Worklight's Logger (for cleaner LogCat output)
 				console.debug = WL.Logger.debug;
 				console.log   = WL.Logger.debug;
 				console.info  = WL.Logger.debug;
